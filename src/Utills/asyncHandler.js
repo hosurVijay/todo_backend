10e55ef0e790e768621a62146c3d899aa15b0d6d
hosurVijay <hosurvijayendra@gmail.com>
2025-07-26 19:28:18 +0530

    Removed duplicate backend files from root

--- src/Utills/asyncHandler.js
+++
@@ -1,9 +0,0 @@
-const asyncHandler = (requestedHandler) => {
-  return (req, res, next) => {
-    Promise.resolve(requestedHandler(req, res, next)).catch((error) =>
-      next(error)
-    );
-  };
-};
-
-export { asyncHandler };
